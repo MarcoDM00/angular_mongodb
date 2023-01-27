f6bc40b8ecd818608c07c86e1dd52e320f4c916f
MarcoDM00 <marcodemattia00@gmail.com>
2023-01-27 21:16:50 +0000

    progetto finito senza conenssione database

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,6 @@
-import { Component } from "@angular/core";
+import { Component} from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { PostsService } from "../posts.service";
 
 @Component({
     selector: 'app-post-create',
@@ -6,10 +8,14 @@ import { Component } from "@angular/core";
     styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-    inputValue = "";
-    newPost = "NO CONTENT";
+    inputTitle = ''
+    inputContent = "";
 
-    onAddPost(postInput: HTMLTextAreaElement) {
-        this.newPost = this.inputValue;
+    constructor(public postsService: PostsService) {}
+
+    onAddPost(form: NgForm) {
+        if (form.invalid) return;
+        this.postsService.addPost(form.value.title, form.value.content);
+        form.resetForm();
     }
 }
