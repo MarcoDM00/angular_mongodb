3872e68b0fc389c28e8a4034f2e0f67cb763a021
MarcoDM00 <demattia.marco@iisgalvanimi.edu.it>
2023-02-02 19:11:58 +0100

    3

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -25,7 +25,6 @@ export class PostListComponent implements OnInit {
   update(i: number) {
     if (this.updateInfos == null) {
       this.updateInfos = setTimeout(() => {
-        console.log("chiamata update")
         var p = this.posts[i];
         this.postsService.update(p.id, p.like, p.dislike, p.commenti);
         this.updateInfos = null;
